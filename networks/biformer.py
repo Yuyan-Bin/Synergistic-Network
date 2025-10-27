d76dde4ddfb131a838c117f26a8e256d9e2bab47
Yuyan-Bin <y4979592@gmail.com>
2025-10-27 13:23:57 +0800

    Update biformer.py

--- networks/biformer.py
+++ networks/biformer.py
@@ -8,7 +8,6 @@ email: ray.leizhu@outlook.com
 This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
-# ***********************************************?
 import os
 
 from bra_block import DWConv
@@ -31,73 +30,31 @@ class Block(nn.Module):
         # modules
         if before_attn_dwconv > 0:
             self.pos_embed = nn.Conv2d(dim, dim, kernel_size=before_attn_dwconv, padding=1, groups=dim)
-            # self.pos_embed =  InceptionNeXtBlock(dim)
         else:
             self.pos_embed = lambda x: 0
         self.norm1 = nn.LayerNorm(dim, eps=1e-6)  # important to avoid attention collapsing
         if topk > 0:
-            # self.attn = BiLevelRoutingAttention(dim=dim, num_heads=num_heads, n_win=n_win, qk_dim=qk_dim,
-            #                                     qk_scale=qk_scale, kv_per_win=kv_per_win,
-            #                                     kv_downsample_ratio=kv_downsample_ratio,
-            #                                     kv_downsample_kernel=kv_downsample_kernel,
-            #                                     kv_downsample_mode=kv_downsample_mode,
-            #                                     topk=topk, param_attention=param_attention, param_routing=param_routing,
-            #                                     diff_routing=diff_routing, soft_routing=soft_routing,
-            #                                     side_dwconv=side_dwconv,
-            #                                     auto_pad=auto_pad)
-            # # **************************************************************************************************************
-            # self.attn2 = DeBiLevelRoutingAttention(dim=dim, num_heads=num_heads, n_win=n_win, qk_dim=qk_dim,
-            #                                        qk_scale=qk_scale, kv_per_win=kv_per_win,
-            #                                        kv_downsample_ratio=kv_downsample_ratio,
-            #                                        kv_downsample_kernel=kv_downsample_kernel,
-            #                                        kv_downsample_mode=kv_downsample_mode,
-            #                                        topk=topk, param_attention=param_attention,
-            #                                        param_routing=param_routing,
-            #                                        diff_routing=diff_routing, soft_routing=soft_routing,
-            #                                        side_dwconv=side_dwconv,
-            #                                        auto_pad=auto_pad, param_size='tiny')
-            # self.attn = AgentAttention(dim=dim, num_heads=num_heads, n_win=n_win, qk_dim=qk_dim,
-            #                                     qk_scale=qk_scale, kv_per_win=kv_per_win,
-            #                                     kv_downsample_ratio=kv_downsample_ratio,
-            #                                     kv_downsample_kernel=kv_downsample_kernel,
-            #                                     kv_downsample_mode=kv_downsample_mode,
-            #                                     topk=topk, param_attention=param_attention, param_routing=param_routing,
-            #                                     diff_routing=diff_routing, soft_routing=soft_routing,
-            #                                     side_dwconv=side_dwconv,
-            #                                     auto_pad=auto_pad, num_patches=input_resolution[0]*input_resolution[1])
-            # self.attn = AggregatedAttention(dim=dim, num_heads=num_heads,input_resolution=input_resolution)
-            # self.attn = CPCA(in_channels=dim, out_channels=dim)
             self.attn = CGLU(in_features=dim)
-            # ********************************************************************************************************************
         elif topk == -1:
             self.attn = Attention(dim=dim)
         elif topk == -2:
             self.attn = AttentionLePE(dim=dim, side_dwconv=side_dwconv)
         elif topk == 0:
-            self.attn = nn.Sequential(rearrange('n h w c -> n c h w'),  # compatiability
-                                      nn.Conv2d(dim, dim, 1),  # pseudo qkv linear
-                                      nn.Conv2d(dim, dim, 5, padding=2, groups=dim),  # pseudo attention
-                                      nn.Conv2d(dim, dim, 1),  # pseudo out linear
+            self.attn = nn.Sequential(rearrange('n h w c -> n c h w'),
+                                      nn.Conv2d(dim, dim, 1), 
+                                      nn.Conv2d(dim, dim, 5, padding=2, groups=dim), 
+                                      nn.Conv2d(dim, dim, 1), 
                                       rearrange('n c h w -> n h w c')
                                       )
         self.norm2 = nn.LayerNorm(dim, eps=1e-6)
 
         self.mlp = nn.Sequential(nn.Linear(dim, int(mlp_ratio * dim)),
                                  DWConv(int(mlp_ratio * dim)) if mlp_dwconv else nn.Identity(),
-                                 # InceptionNeXtBlock(int(mlp_ratio * dim)) if mlp_dwconv else nn.Identity(),
                                  nn.GELU(),
                                  nn.Linear(int(mlp_ratio * dim), dim)
                                  )
 
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-# ***************************************************************************************************************************
-#         self.drop_path2 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-#
-#         self.norm3 = nn.LayerNorm(dim, eps=1e-6)
-#         self.norm4 = nn.LayerNorm(dim, eps=1e-6)
-#
-#         self.mlp2 = TransformerMLPWithConv(dim, mlp_ratio, 0.)
-# ******************************************************************************************************************************
 
         # tricks: layer scale & pre_norm/post_norm
         if layer_scale_init_value > 0:
@@ -131,56 +88,17 @@ class Block(nn.Module):
         if self.pre_norm:
             if self.use_layer_scale:
                 x = x + self.drop_path(self.gamma1 * self.attn(self.norm1(x)))  # (N, H, W, C)
-                # *************************************************************************************
-                # x = x + self.drop_path(self.gamma1 * self.attn(self.norm1(x),H, W, relative_pos_index, relative_coords_table))  # (N, H, W, C)
-                # *************************************************************************************
-                x = x + self.drop_path(self.gamma2 * self.WF(self.norm2(x)))  # (N, H, W, C)
-
-                # *******************************************************************************
-                # # conv pos embedding
-                # x = x + self.pos_embed2(x.permute(0, 3, 1, 2)).permute(0, 2, 3, 1)
-                #
-                # x = x + self.drop_path2(self.gamma3 * self.attn2(self.norm3(x)))  # (N, H, W, C)
-                # x = x + self.drop_path2(self.gamma4 * self.mlp2(self.norm4(x)))  # (N, H, W, C)
-                # ************************************************************************************
+                x = x + self.drop_path(self.gamma2 * self.WF(self.norm2(x))) 
             else:
-                x = x + self.drop_path(self.attn(self.norm1(x)))  # (N, H, W, C)
-                # x = x + self.drop_path(self.attn(self.norm1(x),H, W, relative_pos_index, relative_coords_table))  # (N, H, W, C)
-                x = x + self.drop_path(self.mlp(self.norm2(x)))  # (N, H, W, C)
-
-                # *******************************************************************************
-                # # conv pos embedding
-                # x = x + self.pos_embed2(x.permute(0, 3, 1, 2)).permute(0, 2, 3, 1)
-                #
-                # x = x + self.drop_path2(self.gamma3 * self.attn2(self.norm3(x)))  # (N, H, W, C)
-                # x = x + self.drop_path2(self.gamma4 * self.mlp2(self.norm4(x)))  # (N, H, W, C)
-                # ************************************************************************************
-
+                x = x + self.drop_path(self.attn(self.norm1(x))) 
+                x = x + self.drop_path(self.mlp(self.norm2(x)))  
         else:  # https://kexue.fm/archives/9009
             if self.use_layer_scale:
-                x = self.norm1(x + self.drop_path(self.gamma1 * self.attn(x)))  # (N, H, W, C)
-                x = self.norm2(x + self.drop_path(self.gamma2 * self.WF(x)))  # (N, H, W, C)
-
-                # *****************************************************************************************************
-                # # conv pos embedding
-                # x = x + self.pos_embed2(x.permute(0, 3, 1, 2)).permute(0, 2, 3, 1)
-                #
-                # x = self.norm3(x + self.drop_path2(self.gamma3 * self.attn2(x)))  # (N, H, W, C)
-                # x = self.norm4(x + self.drop_path2(self.gamma4 * self.mlp2(x)))  # (N, H, W, C)
-                # ********************************************************************************************************
+                x = self.norm1(x + self.drop_path(self.gamma1 * self.attn(x))) 
+                x = self.norm2(x + self.drop_path(self.gamma2 * self.WF(x)))  
             else:
-                x = self.norm1(x + self.drop_path(self.attn(x)))  # (N, H, W, C)
-                x = self.norm2(x + self.drop_path(self.WF(x)))  # (N, H, W, C)
-
-                # **************************************************************************************************************
-                # # conv pos embedding
-                # x = x + self.pos_embed2(x.permute(0, 3, 1, 2)).permute(0, 2, 3, 1)
-                #
-                # x = self.norm3(x + self.drop_path2(self.attn2(x)))  # (N, H, W, C)
-                # x = x + self.drop_path2(
-                #     self.mlp2(self.norm4(x).permute(0, 3, 1, 2)).permute(0, 2, 3, 1))  # (N, H, W, C)
-                # ****************************************************************************************************************
-
+                x = self.norm1(x + self.drop_path(self.attn(x)))
+                x = self.norm2(x + self.drop_path(self.WF(x)))  
 
         # permute back
         x = x.permute(0, 3, 1, 2)  # (N, H, W, C) -> (N, C, H, W)
